5e00f6ee550212dd627ed6f8b5a3c8a99b89c5ac
projectKody <pinky29196@gmail.com>
2021-08-06 09:53:47 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a0db0e4b29082cf5ebbbdf4f9fc6c4c7",
-"/": "a0db0e4b29082cf5ebbbdf4f9fc6c4c7",
+"index.html": "bc80c2d9f9955a7766baff463ccefaae",
+"/": "bc80c2d9f9955a7766baff463ccefaae",
 "main.dart.js": "fdfecadc419287941ff152e291208d73",
 "manifest.json": "93465fa57d4535dc57f667f4eb1b9304",
 "version.json": "fe3614bc69b6d5841fdc4da774f9fa71"
